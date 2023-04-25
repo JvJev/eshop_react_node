b3755e95d7ae8d138095d7d0e9d09e89e45eaa68
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2023-04-25 23:11:30 +0300

    refreshing-react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href= "/">Jev's eshop</a>
+        <main>Products list</main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
