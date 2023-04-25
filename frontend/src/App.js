037de2f2e10308ba094a806064e0adfe6a21191e
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2023-04-26 00:00:09 +0300

    some_responsive_components_w_static_data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
         <a href= "/">Jev's eshop</a>
-        <main>Products list</main>
-      </header>
+        </header>
+        <main>Products list
+          <h1>Featured products</h1>
+          <div className="products">
+          {data.products.map(product =>(<div className="product" key={product.slug}>
+            
+              <img src={product.image} alt={product.name}/>
+              
+              <div className="product-info">
+                <p>
+                  {product.name}
+                </p>
+                <p>
+                  {product.price}
+                </p>
+                <button>Add to cart</button> 
+              </div>      
+            </div>))}
+          </div>
+
+          
+
+        </main>
+      
     </div>
   );
 }
