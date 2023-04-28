ff2f1854c462a2e73dd210470c6b2d1e6103d61c
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2023-04-29 00:24:59 +0300

    basic_bootstrap_and_bug_fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,34 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/homescreen';
 import ProductPage from './Pages/ProductPage';
+import NavBar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to="/">Jev's eshop</Link>
+          <NavBar bg="dark" variant="dark">
+            <Container>
+              <NavBar.Brand>
+                <Link to="/">Jev's eshop</Link>
+              </NavBar.Brand>
+            </Container>
+          </NavBar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductPage />} />
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>All rights reserved to Outstanding Larva</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
