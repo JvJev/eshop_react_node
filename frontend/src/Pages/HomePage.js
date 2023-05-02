04ad3e05421426bf00d49bfbfacd5f0f61c689dc
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2023-05-02 23:03:33 +0300

    product_not_found_errors_added

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ProductComponent from '../components/Product';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -43,9 +45,9 @@ function HomePage() {
       </Helmet>
       <div className="products">
         {loading ? (
-          <div>Loading...</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
             {products.map((product) => (
