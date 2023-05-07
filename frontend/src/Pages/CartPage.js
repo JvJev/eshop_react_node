5d5117f1701c009835672a5aa9a502a7886fe1cd
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2023-05-07 23:28:55 +0300

    mainPage_buttons_activated_(something_is_buggy)

--- frontend/src/Pages/CartPage.js
+++ frontend/src/Pages/CartPage.js
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import MessageBox from '../components/MessageBox';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Store } from '../context/Store';
@@ -11,6 +11,7 @@ import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 
 export default function CartPage() {
+    const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
@@ -19,14 +20,20 @@ export default function CartPage() {
   const updateCartHandler = async (item, quantity) => {
     const { data } = await axios.get(`/api/products/${item._id}`);
     if (data.countInStock < quantity) {
-        window.alert('Sorry, this products is out of stock');
-        return;
-      }
-      ctxDispatch({
-        type: 'CART_ADD_ITEM',
-        payload: { ...item, quantity },
-      });
-  }
+      window.alert('Sorry, this products is out of stock');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
+  const removeItemHandler = (item) => {
+    ctxDispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  };
+const checkOutHandler = () => {
+    navigate('/signin?redirect=/shipping');
+}
 
   return (
     <div>
@@ -53,16 +60,21 @@ export default function CartPage() {
                       ></img>{' '}
                     </Col>
                     <Col md={3}>
-                      <Button variant="light" 
-                                              onClick={() => updateCartHandler(item, item.quantity - 1)}
-
-                      disabled={item.quantity === 1}>
+                      <Button
+                        variant="light"
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity - 1)
+                        }
+                        disabled={item.quantity === 1}
+                      >
                         <i className="fas fa-minus-circle"></i>
                       </Button>{' '}
                       <span>{item.quantity}</span>{' '}
                       <Button
                         variant="light"
-                        onClick={() => updateCartHandler(item, item.quantity + 1)}
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity + 1)
+                        }
                         disabled={item.quantity === item.countInStock}
                       >
                         <i className="fas fa-plus-circle"></i>
@@ -70,7 +82,10 @@ export default function CartPage() {
                     </Col>
                     <Col md={3}>${item.price}</Col>
                     <Col md={2}>
-                      <Button variant="light">
+                      <Button
+                        variant="light"
+                        onClick={() => removeItemHandler(item)}
+                      >
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
@@ -95,6 +110,7 @@ export default function CartPage() {
                     <Button
                       type="button"
                       variant="primary"
+                      onClick={checkOutHandler}
                       disabled={cartItems.lenght === 0}
                     >
                       Procced to checkout
