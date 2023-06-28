14fa10fd56969d722f68a66b15e66096d27fca15
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2023-06-28 21:11:20 +0300

    Form_added

--- frontend/src/Pages/PaymentMethodPage.js
+++ frontend/src/Pages/PaymentMethodPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import CheckoutSteps from '../components/CheckoutSteps';
 import { Helmet } from 'react-helmet-async';
+import Form from 'react-bootstrap/Form';
 
 export default function PaymentMethodPage() {
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
   return (
     <div>
       <CheckoutSteps step1 step2 step3></CheckoutSteps>
@@ -11,6 +16,7 @@ export default function PaymentMethodPage() {
           <title>payment method</title>
         </Helmet>
         <h1 className="my-3">Payment method</h1>
+        <Form onSubmit={submitHandler}></Form>
       </div>
     </div>
   );
